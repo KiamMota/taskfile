52673cc4e863287f63e96b9118de9fbc068b0648
KiamMota <somentekiam@gmail.com>
2025-08-26 20:47:50 -0300

    add : default file

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import os
 import utils.messages
 import utils.file
 
-def get_args():
-    if sys.argv[1] == "create" and sys.argv[2] != "":
-        file: str = sys.argv[2]
-        abspath: str = os.getcwd()
+def create():
+    file: str = sys.argv[2]
+    abspath: str = os.getcwd()
+    if sys.argv[2] == "": return
+
+    if sys.argv[1] == "create" and sys.argv[2] != '-':
         utils.file.create_file(file)
         print("file '" + file + "' created in " + abspath)
+        return
+    if sys.argv[1] == "create" and sys.argv[2] == '-':
+        utils.file.create_file("taskfile.txt")
+        print("file named taskfile.txt created in " + abspath)
+        return
 
 def init():
     if len(sys.argv) < 2:
@@ -20,6 +27,6 @@ def init():
     if sys.argv[1] == "--version" or sys.argv[1] == "-v" :
         print(utils.messages.version_message())
         exit(0)
-    get_args() 
+    create() 
 
 init()
