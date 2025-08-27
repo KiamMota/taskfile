86d6535105cc2a78204782de52a3301b3a70593d
KiamMota <somentekiam@gmail.com>
2025-08-27 18:59:08 -0300

    start

--- cmd/list.py
+++
@@ -1,8 +0,0 @@
-import context
-import utils.file
-import context
-
-def cmd_list():
-    str_list: str = utils.file.get_file_content(context.get_taskfile_context())
-    print("tasks: ")
-    print(str_list)
